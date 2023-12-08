372feafe880fc6cc2120118139f4bd0655fc5ac4
DBoss <mtncn_clk17@hotmail.com>
2023-12-08 16:43:59 +0300

    Api Crud Creating

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,11 +1,20 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection; // IServiceCollection ve GetService metodu için gerekli using bildirimi
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class BaseApiController
+    public class BaseApiController : ControllerBase
     {
+        private readonly IMediator _mediator;
 
+        public BaseApiController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        protected IMediator Mediator => _mediator;
     }
 }
